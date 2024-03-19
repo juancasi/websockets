7964008b3fad89dd22b7b396faff600d3b903682
JUAN ANTONIO CASTRO SILVA <juan.castro@usco.edu.co>
2024-03-19 10:10:11 -0500

    Add files via upload
    first change

---
+++ index.js
@@ -0,0 +1,54 @@
+import {WebSocketServer} from 'ws';
+//https://blog.postman.com/set-up-a-websockets-server-in-node-js-postman/
+
+const wss = new WebSocketServer({port:8080});
+
+var gpu_client = {};
+
+var clients = [];
+wss.on('connection', function connection(ws) {
+  console.log("connected");
+  clients.push(ws);
+  ws.on('message', function message(data) {
+    
+    //console.log(data);
+
+    if (data == "GPU-1234"){
+      gpu_client = ws;
+    }
+
+    if (data.toString().startsWith("classify")){
+      console.log("cliente " + data);
+      clients[0].send(data);
+    }
+
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+
+  
+  const intervalID = setInterval(myCallback, 3000, "Parameter 1");
+  function myCallback(p){
+    ws.send(p);
+  }
+  
+
+
+
+  /*
+  clients.foreach(client => {
+    client.send("new something");
+  }
+  );
+  */
+
+
+  /*
+
+  for var i = 0; i< clients.length; i++{
+    clients[i].send('new something' + i);  
+  }
+  */
+
+});
